278ed2b34b8e5abebbb424a72ce70e2bfbcf6113
HudsonPowers <89892390+HudsonPowers@users.noreply.github.com>
2022-01-26 13:06:41 -0800

    Proposal Finished

--- Program.cs
+++ Program.cs
@@ -2,11 +2,29 @@
 
 namespace HighScoreTracker
 {
-    class Program
+    //     class Program
+    //     {
+    //         static void Main(string[] args)
+    //         {
+    //             Console.WriteLine("Hello World!");
+    //         }
+    //     }
+    // }
+
+    static List<int> LoadScoreValues(string filename);
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+    
+    }
+static int GetScoreValue(string score);
+    {
+
+    }
+static int FindInsertionPoint(List<int> values, int newScore);
+    {
+
+    }
+static void AddScore(String name, int score, int insertAt, List<String> scores);
+    {
+
     }
 }

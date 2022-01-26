a1195e232434e6671909664732adfc1080053b65
HudsonPowers <89892390+HudsonPowers@users.noreply.github.com>
2022-01-26 13:09:04 -0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -9,22 +9,18 @@ namespace HighScoreTracker
     //             Console.WriteLine("Hello World!");
     //         }
     //     }
-    // }
-
-    static List<int> LoadScoreValues(string filename);
-    {
+   
+   
+static List<int> LoadScoreValues(string filename);
+{
     
-    }
-static int GetScoreValue(string score);
-    {
-
-    }
-static int FindInsertionPoint(List<int> values, int newScore);
-    {
-
-    }
-static void AddScore(String name, int score, int insertAt, List<String> scores);
-    {
-
-    }
 }
+
+
+// static int GetScoreValue(string score);
+// static int FindInsertionPoint(List<int> values, int newScore);
+// static void AddScore(String name, int score, int insertAt, List<String> scores);
+   
+   
+}
+
